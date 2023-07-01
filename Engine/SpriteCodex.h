da9fadc10f6c20243d52578473fb3346c393a95b
tyrus-tracey <ttracey@sfu.ca>
2023-07-01 12:18:46 +0900

    ball draw's RGB now modifiable

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -7,6 +7,6 @@ class SpriteCodex
 {
 public:
 	// centered drawing of 14x14 sprite
-	static void DrawBall( const Vec2& center,Graphics& gfx );
+	static void DrawBall( const Vec2& center,Graphics& gfx, int plusRed );
 	static void DrawCore(const Vec2& topLeft, Graphics& gfx);
 };
