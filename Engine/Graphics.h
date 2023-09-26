396e46a3072f9e939b2b599215489b0153db16fd
ttracey <ttracey@sfu.ca>
2023-09-26 14:03:04 -0700

    increased graphics windowsize to 1000x1000 (prev. 800x600)

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -78,6 +78,6 @@ private:
 	D3D11_MAPPED_SUBRESOURCE							mappedSysBufferTexture;
 	Color*                                              pSysBuffer = nullptr;
 public:
-	static constexpr int ScreenWidth = 800;
-	static constexpr int ScreenHeight = 600;
+	static constexpr int ScreenWidth = 1000;	// old: 800
+	static constexpr int ScreenHeight = 1000;	// old: 600
 };
