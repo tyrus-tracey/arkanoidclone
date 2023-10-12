ec5f5a5059029e5cc203e67546bac3775c74780c
ttracey <ttracey@sfu.ca>
2023-10-11 20:47:03 -0700

    added sounds

--- Engine/Soundbank.cpp
+++ Engine/Soundbank.cpp
@@ -6,6 +6,12 @@ void Soundbank::brickHit()
 	sBrickHit.Play();
 }
 
+void Soundbank::rockHit()
+{
+	sRockHit.StopAll();
+	sRockHit.Play();
+}
+
 void Soundbank::wallRebound()
 {
 	sWallRebound.StopAll();
@@ -16,4 +22,52 @@ void Soundbank::paddleRebound()
 {
 	sPaddleRebound.StopAll();
 	sPaddleRebound.Play();
+}
+
+void Soundbank::ballExplosion()
+{
+	sBallExplosion.StopAll();
+	sBallExplosion.Play(1.0f, 0.75f);
+}
+
+void Soundbank::coreExplosionMini()
+{
+	sCoreExplosionMini.StopAll();
+	sCoreExplosionMini.Play(3.0f, 0.3f);
+}
+
+void Soundbank::coreExplosionFinal()
+{
+	sCoreExplosionFinal.StopAll();
+	sCoreExplosionFinal.Play();
+}
+
+void Soundbank::glassPlonk()
+{
+	sGlassPlonk.StopAll();
+	sGlassPlonk.Play();
+}
+
+void Soundbank::glassBreak()
+{
+	sGlassBreak.StopAll();
+	sGlassBreak.Play();
+}
+
+void Soundbank::lockBeep()
+{
+	sLockBeep.StopAll();
+	sLockBeep.Play(0.75f, 0.25f);
+}
+
+void Soundbank::metalHit()
+{
+	sMetalHit.StopAll();
+	sMetalHit.Play();
+}
+
+void Soundbank::pkeeuuun()
+{
+	sPkeeuuun.StopAll();
+	sPkeeuuun.Play();
 }
--- Engine/Soundbank.h
+++ Engine/Soundbank.h
@@ -5,12 +5,32 @@ class Soundbank
 {
 public:
 	void brickHit();
+	void rockHit();
 	void wallRebound();
 	void paddleRebound();
+	void ballExplosion();
+	void coreExplosionMini();
+	void coreExplosionFinal();
+	void glassPlonk();
+	void glassBreak();
+	void lockBeep();
+	void metalHit();
+	void pkeeuuun();
 
 
 private:
 	Sound sBrickHit = Sound(L"Sounds\\arkbrick.wav");
+	Sound sRockHit = Sound(L"Sounds\\rockHit.wav");
 	Sound sWallRebound = Sound(L"Sounds\\arkbrick.wav");
 	Sound sPaddleRebound = Sound(L"Sounds\\arkpad.wav");
+	Sound sBallExplosion = Sound(L"Sounds\\ballExplosion.wav");
+	Sound sBounce = Sound(L"Sounds\\bounce.wav");
+	Sound sCoreExplosionMini = Sound(L"Sounds\\coreExplosionMini.wav");
+	Sound sCoreExplosionFinal = Sound(L"Sounds\\coreExplosionFinal.wav");
+	Sound sGlassPlonk = Sound(L"Sounds\\glassPlonk.wav");
+	Sound sGlassBreak = Sound(L"Sounds\\glassBreak.wav");
+	Sound sLockBeep = Sound(L"Sounds\\lockBeep.wav");
+	Sound sMetalHit = Sound(L"Sounds\\metalHit.wav");
+	Sound sPkeeuuun = Sound(L"Sounds\\pkeeuuun.wav");
+
 };
