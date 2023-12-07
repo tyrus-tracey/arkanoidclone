a0cd7d5774a7d9ce42b802bf98e3c2f579cd0c64
tyrus-tracey <ttracey@sfu.ca>
2023-12-07 12:14:36 -0800

    added bounds check to initializing brick in level params

--- Engine/levelBook.h
+++ Engine/levelBook.h
@@ -17,9 +17,11 @@ private:
 	levelParams genLv4();
 	levelParams genLv5();
 
+	bool withinBounds(const levelParams& params, const gridLocation loc) const;
 	int getNumCols(const levelParams& params) const;
 	int getNumRows(const levelParams& params) const;
 
+	void initBrick(levelParams& params, const gridLocation loc, const brickTypeEnum brickType);
 	void initRect(levelParams& params, const gridLocation topLeft, const gridLocation botRight, const brickTypeEnum brickType);
 	void initLineHori(levelParams& params, const int row, const int x1, const int x2, const brickTypeEnum brickType);
 	void initLineVert(levelParams& params, const int col, const int y1, const int y2, const brickTypeEnum brickType);
